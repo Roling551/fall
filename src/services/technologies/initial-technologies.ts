67a06362090eed95a859b323430bdbf60e6739fa
Roling551 <Roling95@gmail.com>
2025-09-02 12:45:22 +0200

    Remove some technologies

--- src/services/technologies/initial-technologies.ts
+++ src/services/technologies/initial-technologies.ts
@@ -11,51 +11,7 @@ const avaliableTechnologies = new Map<string, Technology>([
 export const initialTechnologies:TechnologyTreeItem[] = [
     new Technology("Writing", new Map()),
     new Technology("Masonry", new Map()),
-    new Technology("Farming", new Map([
-        [
-            "unlock_farm",
-            {
-                type: "unlock-estate",
-                estateName: "farm",
-                getEstate: () => new Estate("farm", new Map([["food",2], ["workers-need", 1]]))
-            }
-        ]
-    ])),
-    new Technology("Irrigation", new Map([
-        [
-            "farm_bonus",
-            {
-                type: "estate-production-bonus",
-                bonus: {
-                    type: "estate-production",
-                    name: "Irrigation",
-                    qualifier: (estate: Estate)=>estate.name==="farm",
-                    bonus: (estate: Estate)=>new Map([["food",1]])
-                }
-            }
-        ]
-    ])),
-    new Technology("Mining", new Map([
-        [
-            "unlock-tower",
-                    {
-                name: "unlock-tower",
-                type: "unlock-estate",
-                estateName: "mine",
-                getEstate: () => new Estate("mine", new Map([["gold",1], ["workers-need", 1]]))
-            }
-        ]
-    ])),
-    new Technology("Towers", new Map([
-        [
-            "unlock-mine",
-            {
-                type: "unlock-estate",
-                estateName: "tower",
-                getEstate: () => new Estate("tower", new Map([["authority",5], ["workers-need", 1]]))
-            }
-        ]
-    ])),
+
     new TechnologySelection("Example_selection", new Set(
         [
             avaliableTechnologies.get("tech1")!,
@@ -65,12 +21,8 @@ export const initialTechnologies:TechnologyTreeItem[] = [
 ].map(x=>getTechnologyTreeItem(x)).filter(x=>!!x)
 
 export const initiaTechnologiesParenthood: [string, string][] = [
-    ["Writing", "Farming"],
-    ["Farming", "Irrigation"],
     ["Writing", "Masonry"],
-    ["Masonry", "Towers"],
     ["Writing", "Example_selection"],
-    ["Example_selection", "Mining"],
 ]
 
 export const topTechnologyName = "Writing"
