1fffa8eb3790061aedb58b162b4b61e79f045da9
Roling551 <Roling95@gmail.com>
2025-09-19 15:53:01 +0200

    Clear turnAcctorService at new level

--- src/services/levels.service.ts
+++ src/services/levels.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from "@angular/core";
 import { Level } from "../models/level/level";
 import { createForceSignal } from "../util/force-signal";
 import { CurrentLevelService } from "./current-level.service";
+import { TurnActorsService } from "./turn-actors.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LevelsService {
-    constructor(private currentLevelService: CurrentLevelService) {}
+    constructor(private currentLevelService: CurrentLevelService, private turnActorService: TurnActorsService) {}
     
     nextLevel() {
         this.currentLevelService.level.set(new Level())
+        this.turnActorService.clear()
     }
 }
--- src/services/turn-actors.service.ts
+++ src/services/turn-actors.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from "@angular/core";
 import { TurnActor } from "../models/turn-actor";
+import { createForceSignal } from "../util/force-signal";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TurnActorsService {
-    actors : TurnActor[] = []
+    actors = createForceSignal<TurnActor[]>([])
     addActor(actor: TurnActor) {
-        this.actors.push(actor)
+        this.actors.get().push(actor)
+        this.actors.forceUpdate()
     }
 
     nextTurn() {
-        for(const actor of this.actors) {
+        for(const actor of this.actors.get()) {
             actor.turnAction()
         }
+    }
+
+    clear() {
+        this.actors.set([])
     }
 }
