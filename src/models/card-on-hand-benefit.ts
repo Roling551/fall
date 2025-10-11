0a5dd85c5a2080dd37cb15d4ec685d98ba98c9d7
Roling551 <Roling95@gmail.com>
2025-10-11 13:37:26 +0200

    Add id to cardOnHand benefits

--- src/models/card-on-hand-benefit.ts
+++ src/models/card-on-hand-benefit.ts
@@ -2,6 +2,8 @@ import { SkillMapActionSkillBonus } from "./bonus"
 import { Skill } from "./skill"
 import { Tile } from "./tile/tile"
 
+let idCounter = 0
+
 export type CardOnHandBenefitType = "skill-map-action-skill-bonus"
 
 export type CardOnHandBenefit = SkillMapActionSkillBonus
@@ -9,11 +11,13 @@ export type CardOnHandBenefit = SkillMapActionSkillBonus
 export type CardOnHandBenefits = Map<CardOnHandBenefitType, CardOnHandBenefit>
 
 export function simpleCardOnHandBenefits(cardName: string, skillMapActionSkillBonus: Map<Skill, number>): CardOnHandBenefits {
-    return new Map([["skill-map-action-skill-bonus", {
-            name: "simpleCardOnHandBenefits-"+cardName,
+    const benefits = new Map([["skill-map-action-skill-bonus", {
+            name: "simpleCardOnHandBenefits-"+cardName+"-"+idCounter,
             qualifier: (tile: Tile)=>true,
             bonus: skillMapActionSkillBonus
         }
     ]])
+    idCounter += 1
+    return benefits as CardOnHandBenefits
 }
 
