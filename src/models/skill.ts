efbf9cafd413ac5aec7224a15b984ec48bfae7e0
Roling551 <Roling95@gmail.com>
2026-01-25 20:34:35 +0100

    Add construction graphics. Remove unused skills

--- src/models/skill.ts
+++ src/models/skill.ts
@@ -1,15 +1,11 @@
-export type Skill = "construction" | "science" | "survival"
+export type Skill = "construction"
 
-export const baseZeroSkills = new Map<Skill, number>([["construction", 0], ["science", 0], ["survival", 0]])
+export const baseZeroSkills = new Map<Skill, number>([["construction", 0]])
 
 export function getSkillSymbol(skill: Skill) {
     switch(skill) {
     case "construction":
         return "co";
-    case "science":
-        return "sc";
-    case "survival":
-        return "su";
     }
 }
 
@@ -17,12 +13,6 @@ export function skillsToString(skills: Map<Skill, number>) {
     let result = ""    
     if(skills.get("construction") && skills.get("construction")!>0) {
         result += "co" + skills.get("construction") + " "
-    }
-    if(skills.get("science") && skills.get("science")!>0) {
-        result += "sc" + skills.get("science") + " "
-    }
-    if(skills.get("survival") && skills.get("survival")!>0) {
-        result += "su" + skills.get("survival") + " "
     }
     
     return result
