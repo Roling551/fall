05402181a3a5ffef561275b5bf7fc0e298c3e488
Roling551 <Roling95@gmail.com>
2026-03-10 12:49:13 +0100

    Switch default language to english

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
     constructor(private translate: TranslateService) {
-        translate.use('pl');
+        translate.use('en');
     }
     title = 'fall';
 }
